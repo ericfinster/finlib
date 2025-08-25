a911cdc83ee36a9c2f148b192f7bfbc0692e24ea
Eric Finster <ericfinster@gmail.com>
2025-08-25 15:54:35 +0100

    simpler pp for quote

--- lib/quote.ml
+++ lib/quote.ml
@@ -1,7 +1,6 @@
 (* open Core *)
 
 (* This is the thetadata desciption.  Probably want something more generic *)
-
 type t =
   { ms_of_day : int
   ; bid_size : int
@@ -15,24 +14,5 @@ type t =
   ; date : int
   } 
 
-(* let of_json json = *)
-(*   let open Yojson.Safe.Util in *)
-(*   let quote = to_list json in *)
-(*   match quote with *)
-(*   | [ms;bs;bc;b;be;asz;ac;a;ae;d] -> *)
-(*     { ms_of_day     = to_int ms *)
-(*     ; bid_size      = to_int bs  *)
-(*     ; bid_condition = to_int bc *)
-(*     ; bid           = to_float b *)
-(*     ; bid_exchange  = to_int be *)
-(*     ; ask_size      = to_int asz *)
-(*     ; ask_condition = to_int ac *)
-(*     ; ask           = to_float a *)
-(*     ; ask_exchange  = to_int ae *)
-(*     ; date          = to_int d *)
-(*     }  *)
-(*   | _ -> failwith "parse error" *)
-
-let pp ppf q =
-  Fmt.pf ppf "ms=%d bid=%0.2f ask=%0.2f" q.ms_of_day q.bid q.ask 
+let pp ppf q = Fmt.pf ppf "Bid=%0.2f Ask=%0.2f" q.bid q.ask 
 
