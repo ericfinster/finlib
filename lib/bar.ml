550850119ed93d5c899144544dd96c78f80d077e
Eric Finster <ericfinster@gmail.com>
2025-08-25 17:05:56 +0100

    add bar size type

--- lib/bar.ml
+++ lib/bar.ml
@@ -30,3 +30,26 @@ let pp ppf bar = Fmt.(
       (bar.op :> float) (bar.hi :> float) (bar.lo :> float) (bar.cl :> float)
   )
 
+
+module Size = struct
+  
+  type t =
+    | One_sec     | Five_sec   | Fifteen_sec | Thirty_sec
+    | One_min     | Two_min    | Three_min   | Five_min
+    | Fifteen_min | Thirty_min | One_hour    | One_day
+
+  let to_span = function
+    | One_sec -> Time_float_unix.Span.second
+    | Five_sec -> Time_float_unix.Span.of_sec 5.
+    | Fifteen_sec -> Time_float_unix.Span.of_sec 15.
+    | Thirty_sec -> Time_float_unix.Span.of_sec 30.
+    | One_min -> Time_float_unix.Span.minute
+    | Two_min -> Time_float_unix.Span.of_min 2.
+    | Three_min -> Time_float_unix.Span.of_min 3.
+    | Five_min -> Time_float_unix.Span.of_min 5.
+    | Fifteen_min -> Time_float_unix.Span.of_min 15.
+    | Thirty_min -> Time_float_unix.Span.of_min 30.
+    | One_hour -> Time_float_unix.Span.hour
+    | One_day -> Time_float_unix.Span.day
+                    
+end
