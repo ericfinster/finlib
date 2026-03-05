3da9d0e107af99aea470a2bc5bc86975f896bb3d
Eric Finster <ericfinster@gmail.com>
2026-03-05 11:27:01 +0000

    printing instruments, es index

--- lib/instrument.ml
+++ lib/instrument.ml
@@ -6,7 +6,12 @@ open Base
     
 type t =
   | Spx
-  | Es
+  | Es of int
   | Strat of string * Option_strategy.t 
   [@@deriving hash,sexp,compare,equal,yojson] 
 
+let pp ppf = function
+  | Spx -> Fmt.pf ppf "Spx"
+  | Es d -> Fmt.pf ppf "Es (%d)" d
+  | Strat (date,os) -> Fmt.pf ppf "%s - %a" date Option_strategy.pp os 
+  
